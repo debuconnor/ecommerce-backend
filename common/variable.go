142c8b2adb3e792e286bf9893bc7e2169c923845
debuconnor <gterppw@gmail.com>
2022-03-02 23:23:08 +0900

    common functions
    uint8 to string
    int to string
    float to string
    string to int

--- common/variable.go
+++ common/variable.go
@@ -1,5 +1,21 @@
 package common
 
-func ToString(data interface{}) string{
+import "strconv"
+
+func ResultToString(data interface{}) string{
     return string(data.([]uint8))
+}
+
+func IntToString(data int) string{
+    return strconv.Itoa(data)
+}
+
+func StringToInt(data string) int{
+    num, _ := strconv.Atoi(data)
+
+    return num
+}
+
+func FloatToString(data float64) string{
+    return strconv.FormatFloat(data, 'f', -1, 32)
 }
